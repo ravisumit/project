e14404c64237d2401c4f9e2ca14638f4974be889
ravisumit <82299139+ravisumit@users.noreply.github.com>
2022-11-25 00:06:52 +0530

    Create table.php

---
+++ table.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "demo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE contact_details (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+COMPANY NAME VARCHAR(20) NOT NULL,
+FIRST NAME VARCHAR(15) NOT NULL,
+LAST NAME VARCHAR(15) NOT NULL,
+PHONE int(10) NOT NULL,
+EMAIL varchar(30) NOT NULL,
+COMMENTS varchar(100) 
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table Registration created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
