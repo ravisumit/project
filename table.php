cb0b09026518629701bb04784dfd66e32fc95cb4
ravisumit <82299139+ravisumit@users.noreply.github.com>
2022-11-25 00:24:02 +0530

    Update table.php

--- table.php
+++ table.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "demo";
+$dbname = "file";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
